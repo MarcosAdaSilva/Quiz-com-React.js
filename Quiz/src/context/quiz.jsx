21b3012c42f6d3976daf626319b07c48e14927b0
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-15 00:24:20 -0300

    Trabalhando

--- Quiz/src/context/quiz.jsx
+++ Quiz/src/context/quiz.jsx
@@ -1,4 +1,23 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from "../data/questions";
+
+const STAGES = ["Start", "Playing", "End"];
+
+const initialState = {
+  gameStage: STAGES[0],
+  questions,
+};
+
+const quizReducer = (state, action) => {
+  console.log(state, action);
+  switch (action.type) {
+    case "CHANGE_STATE":
+      return state;
+
+    default:
+      return state;
+  }
+};
 
 export const QuizContext = createContext();
 
