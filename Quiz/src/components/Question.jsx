8075ef027b4bafaf12c339d1a3b59afbc54071fe
MarcosAdaSilva <marcaofly@gmail.com>
2022-09-30 09:58:36 -0300

    Trabalhando

--- Quiz/src/components/Question.jsx
+++ Quiz/src/components/Question.jsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+
 import { QuizContext } from "../context/quiz";
 
 import Option from "./Option";
@@ -8,12 +9,15 @@ import "./Question.css";
 const Question = () => {
   const [quizState, dispatch] = useContext(QuizContext);
   const currentQuestion = quizState.questions[quizState.currentQuestion];
+
   const onSelectOption = (option) => {
     dispatch({
       type: "CHECK_ANSWER",
       payload: { answer: currentQuestion.answer, option },
     });
   };
+
+  console.log(quizState.optionToHide);
 
   return (
     <div id="question">
@@ -28,9 +32,23 @@ const Question = () => {
             key={option}
             answer={currentQuestion.answer}
             selectOption={() => onSelectOption(option)}
+            hide={quizState.optionToHide === option ? "hide" : null}
           />
         ))}
       </div>
+      {!quizState.answerSelected && !quizState.help && (
+        <>
+          {currentQuestion.tip && (
+            <button onClick={() => dispatch({ type: "SHOW_TIP" })}>Dica</button>
+          )}
+          <button onClick={() => dispatch({ type: "REMOVE_OPTION" })}>
+            Excluir uma
+          </button>
+        </>
+      )}
+      {!quizState.answerSelected && quizState.help === "tip" && (
+        <p>{currentQuestion.tip}</p>
+      )}
       {quizState.answerSelected && (
         <button onClick={() => dispatch({ type: "CHANGE_QUESTION" })}>
           Continuar
