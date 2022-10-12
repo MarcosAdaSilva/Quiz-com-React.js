0a2daf2f75f85c2363c4f04f9ce0b151e7da28a5
MarcosAdaSilva <marcaofly@gmail.com>
2022-10-12 20:50:14 -0300

    trabalhando

--- Quiz/src/components/Welcome.jsx
+++ Quiz/src/components/Welcome.jsx
@@ -1,9 +1,9 @@
 import { useContext } from "react";
 import { QuizContext } from "../context/quiz";
 
-import Quiz from "../img/quiz.svg";
+import "./Welcome.css";
 
-import "./welcome.css";
+import Quiz from "../img/quiz.svg";
 
 const Welcome = () => {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -12,10 +12,10 @@ const Welcome = () => {
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar:</p>
-      <button onClick={() => dispatch({ type: "CHANGE_STATE" })}>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
         Iniciar
       </button>
-      <img src={Quiz} alt="Inicio do Quiz" />
+      <img src={Quiz} alt="Início do Quiz" />
     </div>
   );
 };
