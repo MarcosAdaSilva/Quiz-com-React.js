f12954f6fa468f44b36bab88a00f7bb6328e4a5e
MarcosAdaSilva <marcaofly@gmail.com>
2022-10-12 20:46:44 -0300

    Trabalhando

--- Quiz/src/components/GameOver.jsx
+++ Quiz/src/components/GameOver.jsx
@@ -1,7 +1,8 @@
 import { useContext } from "react";
+
 import { QuizContext } from "../context/quiz";
 
-import WellDone from "../img/Welldone.svg";
+import WellDone from "../img/welldone.svg";
 
 import "./GameOver.css";
 
@@ -10,7 +11,7 @@ const GameOver = () => {
 
   return (
     <div id="gameover">
-      <h2>Fim de Jogo!</h2>
+      <h2>Fim de jogo!</h2>
       <p>Pontuação: {quizState.score}</p>
       <p>
         Você acertou {quizState.score} de {quizState.questions.length}{" "}
--- Quiz/src/components/Option.jsx
+++ Quiz/src/components/Option.jsx
@@ -1,14 +1,24 @@
 import { useContext } from "react";
+
 import { QuizContext } from "../context/quiz";
 
 import "./Option.css";
 
-const Option = ({ option, selectOption, answer }) => {
+const Option = ({ option, selectOption, answer, hide }) => {
   const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <div className="option" onClick={() => selectOption()}>
-      <p>{Option}</p>
+    <div
+      onClick={() => selectOption()}
+      className={`
+      option
+        ${quizState.answerSelected && option === answer ? "correct" : ""} ${
+        quizState.answerSelected && option !== answer ? "wrong" : ""
+      }
+        ${hide ? "hide" : ""}
+        `}
+    >
+      <p>{option}</p>
     </div>
   );
 };
